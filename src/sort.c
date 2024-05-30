62b6e1dbd03aec1c3dfeb68e52020fdaed843997
nekoyade <ktr.km.sch@gmail.com>
2024-05-31 03:41:32 +0900

    Create src/sort

---
+++ src/sort.c
@@ -0,0 +1,10 @@
+/*  Project Name    : A2AF
+ *  File Name       : sort.c
+ *
+ *  2024 Keitaro Kamo (nekoyade)
+ *
+ */
+
+#include "sort.h"
+
+// ...
---
+++ src/sort.h
@@ -0,0 +1,13 @@
+/*  Project Name    : A2AF
+ *  File Name       : sort.h
+ *
+ *  2024 Keitaro Kamo (nekoyade)
+ *
+ */
+
+#ifndef A2AF_SORT_H_
+#define A2AF_SORT_H_
+
+// ...
+
+#endif  // A2AF_SORT_H_
