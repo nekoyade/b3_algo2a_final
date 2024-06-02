4ed09b4a27880cc75379dca305860c484fb36247
nekoyade <ktr.km.sch@gmail.com>
2024-06-03 03:56:34 +0900

    Create MeasureSortingTime()

--- src/main.c
+++ src/main.c
@@ -7,9 +7,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "data.h"
 #include "sort.h"
+
+double MeasureSortingTime(
+        struct City* table, int n,
+        void (*sort)(struct City*, int, char, char),
+        char mode, char order, int trials) {
+    double sec = 0.0;
+    struct timeval start_time, end_time;
+    for (int i = 0; i < trials; ++i) {
+        // Shuffle the table
+        QuickSort(table, n, 'p', 'd');
+        ShuffleTable(table, n, 1u);
+
+        // Measure sorting time
+        gettimeofday(&start_time, NULL);
+        sort(table, n, mode, order);
+        gettimeofday(&end_time, NULL);
+        sec += (
+            (end_time.tv_sec - start_time.tv_sec)
+            + (end_time.tv_usec - start_time.tv_usec)/1000000.0
+        );
+    }
+    return sec/trials;
+}
 
 int main() {
     char filename[] = "data/worldcities.csv";
@@ -19,8 +43,10 @@ int main() {
 
     ReadData(&table, &n, filename);
 
-    ShuffleTable(table, n, 1u);
-    DisplayTable(table, n);
+    printf(
+        "%.9f sec\n",
+        MeasureSortingTime(table, 10, BubbleSort, 'p', 'd', 3));
+    printf("%.9f sec\n", MeasureSortingTime(table, 10, QuickSort, 'p', 'd', 3));
 
     free(table);
     return 0;
